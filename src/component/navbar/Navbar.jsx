7eaa8a632722fc0b2873aebae5e23e18da098d0f
WaseemBhrath <waseembhrath514@gmail.com>
2024-08-06 07:06:51 -0700

    Temp Port

---
+++ src/component/navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className=' bg-[#2699fb] p-4' >
+        <div className='max-w-[1170px]  mx-auto border border-black'>
+            <div className=''>
+              <h2>Waseem Abbas</h2>
+            </div>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+              <li>Resources</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
